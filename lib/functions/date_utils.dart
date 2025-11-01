3707fe0c8ad8112912357fa6bdd1bf5529c69bd2
loic <loic19.ytb@gmail.com>
2025-11-01 20:05:16 +0100

    add function for interaction between calandar and reload

--- lib/functions/date_utils.dart
+++ lib/functions/date_utils.dart
@@ -1,6 +1,16 @@
-import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
 
-String formattedTodayDate() {
-  final now = DateTime.now();
-  return DateFormat("EEEE d/M/yy", 'fr_FR').format(now);
+Future<DateTime?> selectDate(BuildContext context, DateTime initialDate) {
+  return showDatePicker(
+    context: context,
+    initialDate: initialDate,
+    firstDate: DateTime(2000),
+    lastDate: DateTime(2100),
+  );
 }
+
+
+//String formattedTodayDate() {
+//  final now = DateTime.now();
+//  return DateFormat("EEEE d/M/yy", 'fr_FR').format(now);
+//}
--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -1,38 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import '../functions/date_utils.dart';
 import '../functions/page_swiper.dart';
-import '../style/custom_header.dart';  
+import '../style/custom_header.dart';
 import 'page3.dart';
 import 'page4.dart';
 import 'page5.dart';
 
 class Page2 extends StatefulWidget {
   const Page2({super.key});
-
   @override
   State<Page2> createState() => _Page2State();
 }
 
 class _Page2State extends State<Page2> {
-  late String dateString;
+  late DateTime todayDate;
+  late DateTime currentDate;
 
   @override
   void initState() {
     super.initState();
-    final now = DateTime.now();
-    dateString = DateFormat("EEEE d/M/yy", 'fr_FR').format(now);
+    todayDate = DateTime.now();
+    currentDate = todayDate;
+  }
+
+  Future<void> onCalendarPressed() async {
+    DateTime? picked = await selectDate(context, currentDate);
+    if (picked != null) {
+      setState(() {
+        currentDate = picked;
+      });
+    }
+  }
+
+  void onRefreshPressed() {
+    setState(() {
+      currentDate = todayDate;
+    });
   }
 
   void onSettingsPressed() {
     print("Settings pressed");
   }
 
-  void onRefreshPressed() {
-    print("Refresh pressed");
-  }
-
   @override
   Widget build(BuildContext context) {
+    String dateString = DateFormat("EEE, MMM d").format(currentDate);
+
     return Scaffold(
       backgroundColor: const Color(0xFF1A1A1A),
       body: SafeArea(
@@ -40,10 +54,10 @@ class _Page2State extends State<Page2> {
           children: [
             CustomHeader(
               dateString: dateString,
+              onCalendarPressed: onCalendarPressed,
               onButton1Pressed: onSettingsPressed,
               onButton2Pressed: onRefreshPressed,
             ),
-
             Expanded(
               child: PageSwiper(
                 pages: const [
